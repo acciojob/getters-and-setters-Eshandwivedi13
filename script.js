924101487733e930af3f1199f9bbbb2491ddda96
Eshan Dwivedi <79198616+Eshandwivedi13@users.noreply.github.com>
2024-08-20 20:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-//complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	set name(name){
+		this._name = name;
+	}
+	set age(age){
+		this._age = age;
+	}
+	get age(){
+		return this._age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name + " is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(this.name + " is teaching");
+	}
+}
+//console.log(person.age); // Output: 25 (using the getter)
+//person.age = 30; =>  Using the setter to set a new value
+
 
 // Do not change the code below this line
 window.Person = Person;
